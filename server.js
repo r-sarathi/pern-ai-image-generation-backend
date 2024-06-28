e1f62333f0cce72a398c0df9c42b723281fddc98
SARATHI R <sarathirofficial@gmail.com>
2024-06-28 19:22:46 +0530

    configured cloudinary

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { v2 as cloudinary } from "cloudinary";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -8,6 +9,12 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running up on http://localhost:${PORT}`);
