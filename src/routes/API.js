81a542033c1634276be118033dd56d0c10e22623
SARATHI R <sarathirofficial@gmail.com>
2024-06-29 09:16:23 +0530

    added image generation route

--- src/routes/API.js
+++ src/routes/API.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { createPost, fetchPosts } from "../controllers/PostController";
+import { createImage } from "../controllers/DallEController";
 
 const router = Router();
 
 // post routes
 router.get("/fetchPosts", fetchPosts);
 router.post("/createPost", createPost);
+
+// ai routes
+router.post("/generateImage", createImage);
+
+export default router;
